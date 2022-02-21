5e160854164238ae1896d648b4d4edf0b06047bd
SherryImtiaz <80238074+SherryImtiaz@users.noreply.github.com>
2022-02-21 16:18:45 +0500

    Update jiraCreateIssue.groovy

--- vars/jiraCreateIssue.groovy
+++ vars/jiraCreateIssue.groovy
@@ -7,8 +7,8 @@ def call(Map config=[:]) {
     issueTypeName: "${config.issueTypeName}"
   ]
   def render = renderTemplate(rawBody,binding)
- // def render2 = render.replaceAll("\\\\n","")
-  def cmd = """curl -D- -u $JIRA_CREDENTIALS -X POST --data \"${render}\" -H \"Content-Type: application/json\" $JIRA_URL/rest/api/2/issue"""
+ def render2 = render.replaceAll("[\\t\\n\\r\\f\\v]","")
+  def cmd = """curl -D- -u $JIRA_CREDENTIALS -X POST --data \"${render2}\" -H \"Content-Type: application/json\" $JIRA_URL/rest/api/2/issue"""
   echo cmd
 // bat(script:cmd)
  
